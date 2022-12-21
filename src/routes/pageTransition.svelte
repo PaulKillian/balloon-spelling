4a3a47f4febe6ae5c91787baf38ee3f5f7c7506f
Paul_Killian <psk65lava@gmail.com>
2022-12-21 10:03:01 -0800

    Update pageTransition.svelte

--- src/routes/pageTransition.svelte
+++ src/routes/pageTransition.svelte
@@ -3,8 +3,8 @@
 </script>
   
 <div
-    in:fly="{{ y: 650, duration: 1050, delay: 700 }}"
-    out:fly="{{ y: -950, duration: 1050 }}" 
+    in:fly="{{ x: 650, duration: 1050, delay: 700 }}"
+    out:fly="{{ x: -950, duration: 1050 }}" 
     >
     <slot/>
 </div>
