4266b7e1d23e572755cc7c139c094579a57eb521
Paul_Killian <psk65lava@gmail.com>
2022-12-21 08:43:00 -0800

    Update handleClick.svelte

--- src/routes/handleClick.svelte
+++ src/routes/handleClick.svelte
@@ -33,6 +33,7 @@
         string=string.replaceAll(',','');
         tl.to(`#${letter}`, {
         rotationY: 27, y: -250, duration: .5,
+	rotationY: 27, x: -250, duration: .5,
         onComplete() {
           string === word && group.forEach((element) => {
             console.log(element)
